1099dd092989b5d806a304cd46eabf61999529fa
Haozhe-py <xuhaozhepy@outlook.com>
2026-08-25 21:45:20 +0800

    Update

--- train/modeling/attn.py
+++ train/modeling/attn.py
@@ -1,15 +1,16 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 from transformers.modeling_utils import ALL_ATTENTION_FUNCTIONS, TransformersKwargs
 from typing import Unpack
 
+from modeling.kernel import attn
+
 def repeat_kv(hidden_states: torch.Tensor, n_rep: int) -> torch.Tensor:
-    """
+    '''
     This is the equivalent of torch.repeat_interleave(x, dim=1, repeats=n_rep). The hidden states go from (batch,
     num_key_value_heads, seqlen, head_dim) to (batch, num_attention_heads, seqlen, head_dim)
-    """
+    '''
     batch, num_key_value_heads, slen, head_dim = hidden_states.shape
     if n_rep == 1:
         return hidden_states
@@ -25,21 +26,24 @@ def attention(
     attention_mask: torch.Tensor | None,
     scaling: float,
     dropout: float = 0.0,
-    **kwargs: Unpack[TransformersKwargs],
+    **_: Unpack[TransformersKwargs],
 ):
+    '''
+    Flash-Attention Interface.
+    dropout must be 0.0, as flash-attention does not support dropout.
+    '''
+
+    assert dropout == 0.0, 'Dropout is not supported in flash-attention'
     key_states = repeat_kv(key, module.num_key_value_groups)
     value_states = repeat_kv(value, module.num_key_value_groups)
 
-    attn_weights = torch.matmul(query, key_states.transpose(2, 3)) * scaling
-    if attention_mask is not None:
-        attn_weights = attn_weights + attention_mask
-
-    attn_weights = F.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query.dtype)
-    attn_weights = F.dropout(attn_weights, p=dropout, training=module.training)
-    attn_output = torch.matmul(attn_weights, value_states)
-    attn_output = attn_output.transpose(1, 2).contiguous()
-
-    return attn_output, attn_weights
+    return attn(
+        query=query,
+        key=key_states,
+        value=value_states,
+        attention_mask=attention_mask,
+        scaling=scaling,
+    )
 
 
 ALL_ATTENTION_FUNCTIONS['eternity-attention'] = attention
